2b357bf5823837908f0fe04a22b47deaf9e3da4e
jackie840129 <jackieliu@media.ee.ntu.edu.tw>
2021-06-18 22:36:58 +0800

    scripts

--- scripts/AA_M.sh
+++ scripts/AA_M.sh
@@ -1,4 +1,4 @@
-python3 tools/train.py --config_file='configs/video_baseline.yml' MODEL.DEVICE_ID "('1,0')" DATASETS.NAMES "('mars',)" INPUT.SEQ_LEN 6 \
+python3 tools/train.py --config_file='configs/video_baseline.yml' MODEL.DEVICE_ID "('0,1')" DATASETS.NAMES "('mars',)" INPUT.SEQ_LEN 6 \
                                                                    OUTPUT_DIR "./ckpt_DL_M/MARS_DL_s6_resnet_axial_gap_rqkv_gran4" SOLVER.SOFT_MARGIN True \
                                                                    MODEL.NAME 'resnet50_axial' MODEL.TEMP 'Done' MODEL.IF_LABELSMOOTH 'no'  INPUT.IF_RE True \
                                                                    DATASETS.ROOT_DIR '/work/sychien421/Dataset/'
